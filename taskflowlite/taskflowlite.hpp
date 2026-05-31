b664597c1b36ccacd1bd8a9865b81123a3942b24
qq978358810 <qq978358810@gmail.com>
2026-05-31 22:56:50 +0800

    fix: 更新版本号2.0.0

--- taskflowlite/taskflowlite.hpp
+++ taskflowlite/taskflowlite.hpp
@@ -27,8 +27,8 @@
 // ==============================================================================
 // 编译期版本宏 (Source of Truth：源码即真理，CMake 会自动来读取这里)
 // ==============================================================================
-#define TASKFLOWLITE_VERSION_MAJOR 1
-#define TASKFLOWLITE_VERSION_MINOR 2
+#define TASKFLOWLITE_VERSION_MAJOR 2
+#define TASKFLOWLITE_VERSION_MINOR 0
 #define TASKFLOWLITE_VERSION_PATCH 0
 
 namespace tfl {
